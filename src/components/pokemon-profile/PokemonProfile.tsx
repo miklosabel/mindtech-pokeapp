457235e09f09644ec05ea144a20e3013d245c041
miklosabel <miklosabel@gmail.com>
2022-06-09 22:01:15 +0200

    pokemon types on profile card

--- src/components/pokemon-profile/PokemonProfile.tsx
+++ src/components/pokemon-profile/PokemonProfile.tsx
@@ -26,54 +26,63 @@ const PokemonProfileModal: FunctionComponent<PokemonProfileProps> = (props: Poke
 	const isOpen = () => props.selectedPokemon !== ''
 	const visibleAbilities = data?.abilities && data.abilities.filter(ability => !ability.is_hidden)
 	const profilePicture = data?.sprites?.front_default
+	const pokemonTypes = data?.types && data.types.map(typeObject => typeObject.type.name)
 
-	return (
+	// render helpers
+	const renderModal = (children: any) => (
 		<Modal
 			open={isOpen()}
 			onClose={() => closeModal()}
 		>
-			<>
-				{isLoading
-					? <CircularProgress />
-					: (
-						<Card sx={{ maxWidth: 345 }}>
-							<CardMedia
-								component="img"
-								image={profilePicture}
-								alt={`profile image of ${data?.name}`}
-							/>
-							<CardContent>
-								<Typography gutterBottom variant="h5" component="div">
-									{data?.name}
-								</Typography>
-									<div>
-										<strong>Weight: </strong>{data?.weight}
-									</div>
-									<div>
-										<strong>Height: </strong>{data?.height}
-									</div>
-									<div>
-										<strong>Visible abilities: </strong>
-										<div>
-											{visibleAbilities && visibleAbilities.map(x =>
-												<div key={x.ability.name}>{x.ability.name}</div>)}
-										</div>
-									</div>
-							</CardContent>
-
-							<Button onClick={() => handlePokemonCatch(props.selectedPokemon)}>
-								{caughtPokemons.includes(props.selectedPokemon) ? 'Release' : 'Catch'}
-							</Button>
-						</Card>
-					)}
-				{error && <div>
-					network related error happened.
-				</div>
-				}
-			</>
+			{children}
 		</Modal>
 	)
 
+	const renderCard = (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardMedia
+				component="img"
+				image={profilePicture}
+				alt={`profile image of ${data?.name}`}
+			/>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="div">
+					{data?.name}
+				</Typography>
+				<div>
+					<strong>Type: </strong>
+					{pokemonTypes?.map(name => <span key={name}>{name + ' '}</span>)}
+				</div>
+				<div>
+					<strong>Weight: </strong>{data?.weight}
+				</div>
+				<div>
+					<strong>Height: </strong>{data?.height}
+				</div>
+				<div>
+					<strong>Visible abilities: </strong>
+					<div>
+						{visibleAbilities && visibleAbilities.map(x =>
+							<div key={x.ability.name}>{x.ability.name}</div>)}
+					</div>
+				</div>
+			</CardContent>
+			<Button onClick={() => handlePokemonCatch(props.selectedPokemon)}>
+				{caughtPokemons.includes(props.selectedPokemon) ? 'Release' : 'Catch'}
+			</Button>
+		</Card>
+	)
+
+	const renderError = (
+		error && <div>
+			network related error happened.
+		</div>
+	)
+
+	// render
+	if (isLoading) return renderModal(<CircularProgress />)
+	else if (error) return renderModal(renderError)
+	else return renderModal(renderCard)
 
 }
 
