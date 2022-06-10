2d10b9d66547f1cb02770515ddcbe206b812d3d8
miklosabel <miklosabel@gmail.com>
2022-06-10 13:48:28 +0200

    removed main component, refactored app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
 import './App.scss';
-import Main from './pages/main/Main';
+import Appbar from './components/appbar/Appbar';
+import PokemonList from './components/pokemon-list/PokemonList';
+import { PokemonTypeNames } from './constants/constants';
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+
+	const renderRoute = (
+		PokemonTypeNames.map(
+			(pokemonType) => (
+				<Route
+					key={pokemonType}
+					path={'/' + pokemonType}
+					element={<PokemonList pokemonType={pokemonType} />}
+				/>
+			)
+		)
+	)
+
+	const renderRoutes = (
+		<Routes>
+			<Route
+				key='main'
+				path="/"
+				element={
+					<><p>Welcome to my pokemon page!</p></>
+				} />
+			{renderRoute}
+		</Routes>
+	)
+
+	return (
+		<div className="App">
+			<Appbar />
+			<div className="content">
+				{renderRoutes}
+			</div>
+		</div>
+	);
 }
 
 export default App;
