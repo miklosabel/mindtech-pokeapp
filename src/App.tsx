f79459c22d11acf52f5805649f9cecd26f40fcf6
miklosabel <miklosabel@gmail.com>
2022-06-19 19:01:44 +0200

    refactor overcomplicated app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,30 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Appbar from "./components/appbar/Appbar";
-import Header from "./components/header/Header";
 import PokemonList from "./components/pokemon-list/PokemonList";
 import { PokemonTypeNames } from "./constants/constants";
+import Header from "./shared/header/Header";
 
 function App(): JSX.Element {
-  const renderRoute = PokemonTypeNames.map((pokemonType) => (
-    <Route
-      key={pokemonType}
-      path={"/" + pokemonType}
-      element={<PokemonList pokemonType={pokemonType} />}
-    />
-  ));
-
-  const renderRoutes = (
-    <Routes>
-      <Route
-        key="landing"
-        path="/"
-        element={<Header>Welcome to my pokeapp!</Header>}
-      />
-      {renderRoute}
-    </Routes>
-  );
-
   return (
     <div className="App">
       <Appbar />
-      <div className="content">{renderRoutes}</div>
+      <div className="content">
+        <Routes>
+          <Route
+            key="landing"
+            path="/"
+            element={<Header>Welcome to my pokeapp!</Header>}
+          />
+          {PokemonTypeNames.map((pokemonType) => (
+            <Route
+              key={pokemonType}
+              path={"/" + pokemonType}
+              element={<PokemonList pokemonType={pokemonType} />}
+            />
+          ))}
+        </Routes>
+      </div>
     </div>
   );
 }
