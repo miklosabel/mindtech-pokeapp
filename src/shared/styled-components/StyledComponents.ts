9a7acefd2bc82d5c9cd2b9b25a8908cacd457f95
miklosabel <miklosabel@gmail.com>
2022-07-06 14:44:58 +0200

    format with prettier

--- src/shared/styled-components/StyledComponents.ts
+++ src/shared/styled-components/StyledComponents.ts
@@ -1,25 +1,25 @@
 import { alpha, Box, FormGroup, InputBase, styled } from "@mui/material";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-export const AppbarOffset = styled('div')(({ theme }) => theme.mixins.toolbar);
+export const AppbarOffset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 export const StyledModalBox = styled(Box)(() => ({
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-}))
+}));
 
 export const StyledHeader = styled("div")`
   display: flex;
   justify-content: center;
   padding-top: 24px;
   padding-bottom: 24px;
-`
+`;
 
 export const StyledNavLink = styled(NavLink)(({ theme }) => ({
   textDecoration: "none",
-  color: alpha(theme.palette.common.black, 0.8)
+  color: alpha(theme.palette.common.black, 0.8),
 }));
 
 export const StyledSearch = styled("div")(({ theme }) => ({
@@ -27,12 +27,12 @@ export const StyledSearch = styled("div")(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25)
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: theme.spacing(3),
   paddingRight: theme.spacing(2),
-  width: '50%',
+  width: "50%",
 }));
 
 export const SearchIconWrapper = styled("div")(({ theme }) => ({
@@ -42,23 +42,22 @@ export const SearchIconWrapper = styled("div")(({ theme }) => ({
   pointerEvents: "none",
   display: "flex",
   alignItems: "center",
-  justifyContent: "center"
+  justifyContent: "center",
 }));
 
 export const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
-  width: '100%',
+  width: "100%",
   "& .MuiInputBase-input": {
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
-  }
+  },
 }));
 
 export const StyledFormGroup = styled(FormGroup)(({ theme }) => ({
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25)
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
-  paddingLeft: theme.spacing(1)
+  paddingLeft: theme.spacing(1),
 }));
-
